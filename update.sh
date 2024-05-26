b6a7fa10f2f62be16235fd4c4de624007258c3be
Ollie-Branch <ollieevelon@gmail.com>
2024-05-26 11:05:44 -0400

    init-commit

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+SCRIPT_DIR=$PWD
+DIRS_TO_ITER=$(ls -al | awk '{print $9}' | sed 1,3d | sed '/update.sh/d')
+NUM_DIRS_TO_ITER=$(printf '%s\n' "$DIRS_TO_ITER" | wc -l)
+
+echo list of dirs to iterate = $DIRS_TO_ITER
+echo number of dirs to iterate = $NUM_DIRS_TO_ITER
+echo current working directory = $SCRIPT_DIR
+
+CURRENT_FOLDER=$(echo $DIRS_TO_ITER | awk '{print $1}')
+CURRENT_DIR=1
+
+echo The first folder is "$CURRENT_FOLDER"
+
+for i in $DIRS_TO_ITER
+do
+	CURRENT_DIR=$i
+	echo $CURRENT_DIR
+	cd $CURRENT_DIR
+	cd ..
+done
